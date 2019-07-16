d3a2067842512e8390706c975b1cffba2df44b18
jsunga <jawyn.sunga@gmail.com>
2019-07-15 19:25:41 -0700

    add pricing section on main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import Why from './Why'
+import Pricing from './Pricing'
 import './styling/main.scss'
 
 const Main = () => {
 
     return (
         <>
-            <div className='main-section'>
+            <div className='landing'>
                 <div>Leave Your Mark</div>
             </div>
-            <div className='main-message'>
+            <div className='message'>
                 Start Your Journery Today!
                 <button>Join Now</button>
             </div>
             <Why />
+            <Pricing />
         </>
     )
 
---
+++ src/components/Pricing.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import './styling/pricing.scss'
+
+const Pricing = () => {
+
+    return (
+        <main className='pricing-section'>
+            <div className='container'>
+                <h1>Pricing</h1>
+                <span>
+                    Pricing structure for 
+                    our services that cannot be beaten.
+                </span>
+                <div>
+                    <section>
+                        <h2>Basic</h2>
+                        <ul>
+                            <li><b>1</b> Pass</li>
+                            <li><b>$10</b> /month</li>
+                            <li><b>Machine</b> Access</li>
+                            <li><b>0</b> Trainers</li>
+                            <li><b>0</b> Perks</li>
+                            <li><button>Sign up</button></li>
+                        </ul>
+                    </section>
+                    <section class='special'>
+                        <h2 class='standard'>STANDARD</h2>
+                        <ul>
+                            <li><b>2</b> Pass</li>
+                            <li><b>$20</b> /month</li>
+                            <li><b>Full</b> Access</li>
+                            <li><b>1</b> Trainer</li>
+                            <li><b>2</b> Perks</li>
+                            <li><button>Sign up</button></li>
+                        </ul>
+                    </section>
+                    <section>
+                        <h2>PRO</h2>
+                        <ul>
+                            <li><b>3</b> Pass</li>
+                            <li><b>$30</b> /month</li>
+                            <li><b>Full</b> Access</li>
+                            <li><b>5</b> Trainer</li>
+                            <li><b>10</b> Perks</li>
+                            <li><button>Sign up</button></li>
+                        </ul>
+                    </section>
+                </div>
+            </div>
+        </main>
+    )
+
+}
+
+export default Pricing
