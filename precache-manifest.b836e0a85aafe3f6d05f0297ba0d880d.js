3d11607694230af28db7b7c7c3532a3c10db4962
jsunga <jawyn.sunga@gmail.com>
2019-07-17 14:18:11 -0700

    Updates

---
+++ precache-manifest.b836e0a85aafe3f6d05f0297ba0d880d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69470aa199e4c202dd97732d6108911a",
+    "url": "/Skyla-Fitness/index.html"
+  },
+  {
+    "revision": "bdde4c2d5d03446f395a",
+    "url": "/Skyla-Fitness/static/css/main.1fed04f4.chunk.css"
+  },
+  {
+    "revision": "85a7a8fabdd5b5bcb912",
+    "url": "/Skyla-Fitness/static/js/2.2c25be81.chunk.js"
+  },
+  {
+    "revision": "bdde4c2d5d03446f395a",
+    "url": "/Skyla-Fitness/static/js/main.b0a2147a.chunk.js"
+  },
+  {
+    "revision": "5fd2e32470bab985063f",
+    "url": "/Skyla-Fitness/static/js/runtime~main.8c45dd7b.js"
+  },
+  {
+    "revision": "8a4c1627711eeb31e14129305ba75702",
+    "url": "/Skyla-Fitness/static/media/bg.8a4c1627.jpg"
+  },
+  {
+    "revision": "4c2691d1656a988668f7007e12195014",
+    "url": "/Skyla-Fitness/static/media/gym1.4c2691d1.jpg"
+  },
+  {
+    "revision": "96770c09e3ca453e740ad0cedda4657d",
+    "url": "/Skyla-Fitness/static/media/gym2.96770c09.jpg"
+  },
+  {
+    "revision": "6b61ffa2c1a7469028a491de313d4770",
+    "url": "/Skyla-Fitness/static/media/gym3.6b61ffa2.jpg"
+  },
+  {
+    "revision": "0acaa2689e8ca5eed926533801d7f84d",
+    "url": "/Skyla-Fitness/static/media/trainer1.0acaa268.png"
+  },
+  {
+    "revision": "de5d8c114b6ceadd61218a7dda9c537c",
+    "url": "/Skyla-Fitness/static/media/trainer2.de5d8c11.png"
+  },
+  {
+    "revision": "a476d731ec5f7ac2e2eb1fe7a4996a21",
+    "url": "/Skyla-Fitness/static/media/trainer3.a476d731.jpg"
+  }
+]);
